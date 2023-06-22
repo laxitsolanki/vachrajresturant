8b89595c9d51647ff56c90541a17ca6f4d14002c
laxitsolanki <solankilaxit963@gmail.com>
2023-06-22 16:48:21 +0530

    header/footer

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Resturant from './component/Resturant';
 function App() {
   return (
     <div>
+    <h1 className='title'>Vachraj resturant</h1>
       <Resturant/>
+      <div className='footer'> design by: laxit solanki</div>
     </div>
   );
 }
